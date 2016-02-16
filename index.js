ba11818c2d96d339fe99879512b9e1871807df85
song940 <song940@gmail.com>
2016-02-16 16:16:17 +0800

    add method .

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 'use strict';
 const pathToRegexp = require('path-to-regexp');
 
-module.exports = function route(def, callback){
+module.exports = function route(method, path, callback){
+  if(!callback){
+    callback = path;
+    path = method;
+    method = null;
+  }
+  if(method){
+    method = method.toUpperCase();
+  }
   var keys = [];
-  var regexp = pathToRegexp(def, keys);
+  var regexp = pathToRegexp(path, keys);
   return function(req, res, next){
-    if(regexp.test(req.url)){
+    if((method ? (method == req.method) : true) &&  regexp.test(req.url)){
       req.params = {};
       var matchs = regexp.exec(req.url).slice(1);
       keys.forEach(function(key, i){
